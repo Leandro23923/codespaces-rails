4a7bd2fc047ea5d28e41c15529f76ea0d7710752
Leandro Ferrari <leandroferrarimunoz@gmail.com>
2024-11-03 14:10:04 +0000

    Ruby

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 class User < ApplicationRecord
+  # Validaciones para asegurarse de que los campos no estén vacíos y sean únicos
   validates :email, presence: true
   validates :name, presence: true
   validates :email, uniqueness: true
+  
+  # Validación personalizada para verificar el dominio del email
   validate :email_domain_must_be_gmail
 
   private
 
+  # Método que implementa la validación personalizada
   def email_domain_must_be_gmail
     return if email.blank? # Permite que el campo esté vacío
 
